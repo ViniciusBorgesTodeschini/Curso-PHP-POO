8c9cff55fa05e4a292538a149222e35f95a37cec
Vinicius Borges Todeschini <75043020+ViniciusBorgesTodeschini@users.noreply.github.com>
2021-01-31 17:04:55 -0300

    update-aula07b

--- Aula07b/Luta.php
+++ Aula07b/Luta.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'Lutador.php';
 class Luta {
     //Atributos
     private $desafiado;
@@ -7,10 +8,48 @@ class Luta {
     private $aprovada;
     
     //Métodos especiais
-    public function marcarLuta(){
-        
+    public function marcarLuta($l1,$l2){
+        if ($l1->getCategoria() == $l2->getCategoria() && $l1!=$l2){
+            $this->aprovada = true;
+            $this->setDesafiado($l1);
+            $this->setDesafiante($l2);
+        } else {
+            $this->aprovada = false;
+            $this->setDesafiado(null);
+            $this->setDesafiante(null);
+        }
     }
     public function lutar(){
+        if ($this->getAprovada()) {
+            $this->getDesafiado()->apresentar();
+            $this->getDesafiante()->apresentar();
+            $vencedor = random_int(0, 2);
+            switch ($vencedor){
+                case 0:
+                    echo "<br>-----------------------------";
+                    echo "<br>Empatou!";
+                    $this->desafiado->empatarLuta();
+                    $this->desafiante->empatarLuta();
+                    break;
+                
+                case 1:
+                    echo "<br>-----------------------------";                    
+                    echo "<br>O lutador desafiado " . $this->desafiado->getNome() . " venceu";
+                     $this->desafiado->ganharLuta();
+                     $this->desafiante->perderLuta();
+                    break;
+                
+                case 2:
+                    echo "<br>-----------------------------";                    
+                    echo "<br>O lutador desafiante " . $this->desafiante->getNome() . " venceu";
+                    $this->desafiante->ganharLuta();
+                    $this->desafiado->perderLuta();
+                    break;                    
+            }
+            
+        } else {
+            echo "<br>Luta não pode acontecer!";
+        }
         
     }
     
--- Aula07b/index.php
+++ Aula07b/index.php
@@ -7,15 +7,23 @@
     <body>
         <?php
             require_once 'Lutador.php';
+            require_once 'Luta.php';
+            
             $L[0] = new Lutador("Pretty boy" , "França", 31, 1.75, 68.9, 11,3,1);
             $L[1] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8,14,2,3);
             $L[2] = new Lutador("Snapshadow", "EUA", 35 , 1.65, 80.9, 12, 2, 1);
             $L[3] = new Lutador("Dead Code", "Austrália", 28, 1.93 , 81.6 , 13 , 0, 2);
             $L[4] = new Lutador("UFOCobol","Brasil", 37, 1.70, 119.3, 5, 4, 3);
             $L[5] = new Lutador("Nerdaart","EUA", 30, 1.81, 105.7, 12, 2, 4);
+
+            echo "CHEGOU A HORA! A grande luta da noite será entre:";
             
-            $L[0]->apresentar();
-            $L[0]->status();
+            $UEC01 = new Luta();
+            $UEC01->marcarLuta($L[2], $L[3]);
+            $UEC01->lutar();
+            
+            $L[2]->status();
+            $L[3]->status();
 
         ?>
     </body>
